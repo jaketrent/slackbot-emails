20f4b6b2472dc4405946774156a6937a488a6a52
jaketrent <trent.jake@gmail.com>
2015-05-13 16:51:13 -0600

    fix port reversal

--- server/index.js
+++ server/index.js
@@ -12,6 +12,6 @@ app.use(bodyParser.json())
 
 routes.map(app)
 
-app.listen(3000 || process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('listening...')
 })
