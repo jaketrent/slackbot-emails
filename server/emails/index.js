7d5678bc3ddb5901cac3d8e22b22504ff0e13de7
jaketrent <trent.jake@gmail.com>
2015-05-14 05:13:36 -0600

    logging

--- server/emails/index.js
+++ server/emails/index.js
@@ -5,13 +5,26 @@ function fmtSlackUrl(token, channelId) {
 }
 
 export function getAllEmails(req, res) {
-  console.log('req', req)
   console.log('req.body', req.body)
 
-
   var channelId = req.body.channel_id
+
+  if (!channelId)
+    return res.status(400).json({ errors: 'channel_id required' })
+
   var token = process.env.SLACK_API_TOKEN
-  request(fmtSlackUrl(token, channelId), function(error, response, body) {
+  var url = fmtSlackUrl(token, channelId)
+  console.log('url', url)
+  request(url, function(error, response, body) {
+    console.log('error', error)
+    console.log('response.statusCode', response.statusCode)
+    console.log('body', body)
+
+    if (error)
+      return res.status(400).json({ error: errror })
+
+
+
     if (!error && response.statusCode == 200 && body.ok === true) {
       body.channel.members.forEach((member) => {
         console.log('mem', member)
